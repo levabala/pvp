8a323cde8f64c2d77904c45daf7515aa3f0d735a
Fisab <vladsudakov5@gmail.com>
2015-12-06 20:51:23 +0300

    Update engine.js

--- engine.js
+++ engine.js
@@ -4,11 +4,12 @@ var w = canvas.width = 1000;
 var h = canvas.height = 800;
 var fisab;
 var findoff;
+var levabala;
 
 function initMap(terrains){
 	terrains.push({
 		type: 'wall',
-		pos: [w, 0],
+		pos: [w - 10, 0],
 		size: [10, h]
 	});
 	terrains.push({
@@ -18,13 +19,18 @@ function initMap(terrains){
 	});
 	terrains.push({
 		type: 'wall',
-		pos: [0, h],
+		pos: [0, h - 10],
+		size: [w-10, h]
+	});
+	terrains.push({
+		type: 'wall',
+		pos: [0, 0],
 		size: [w, 10]
 	});
 	terrains.push({
 		type: 'wall',
-		pos: [0, 0],
-		size: [w, 0]
+		pos: [w/3, h/2],
+		size: [100, 500]
 	});
 }
 
@@ -39,9 +45,9 @@ var tick = 0;
 var oldTick = -1;
 
 var velocity = {
-	gun: Math.PI/70,
+	gun: Math.PI/80,
 	rotate: Math.PI/200,
-	drive: 0.5
+	drive: 3.5
 };
 
 var allInfo = {
@@ -76,9 +82,9 @@ var finishedRequest = [];
 var rqs = 0;
 function init(){
 	fisab = new Worker('ii/fisab.js');
-	//var findoff = new Worker('ai/findoff.js');
+	findoff = new Worker('ii/findoff.js');
 	fisab.postMessage(players[0]);
-	//findoff.postMessage(players[1]);
+	findoff.postMessage(players[1]);
 }
 init();
 fisab.addEventListener('message', function(e) {
@@ -86,26 +92,44 @@ fisab.addEventListener('message', function(e) {
 		players[0].action = e.data.action;
 	}
 }, false);
-/*
+
 findoff.addEventListener('message', function(e) {
 	if(players[0].numbOfAct == players[1].numbOfAct){
 		players[1].action = e.data.action;
 		players[1].radar = e.data.radar;
 	}
 }, false);
-*/
+
 
 function findObjForRadar(pos, angle, player){
-	for(var terrain of terrains){
+		ctx.save();//пример линии
+	    ctx.beginPath();
+	    for(var i = 0; i < w; i++){
+			var x = Math.cos(angle + 0.5*Math.PI);
+			var y = Math.sin(angle + 0.5*Math.PI);
+			ctx.lineTo(x*i+pos[0], y*i+pos[1]);
+		}
+		ctx.stroke();
+		ctx.restore();
 		for(var i = 0; i < w; i++){
-			var x = Math.cos(angle);
-			var y = Math.sin(angle);
-			if(x+i == terrain.pos[0] && y+i == terrain.pos[1]){
-				var xD = x+i - terrain.pos[0];
-				var yD = y+i - terrain.pos[1];
-				var range = Math.sqrt(xD*xD+yD*yD);
-				if(player == 'fisab'){visibleObj.fisab.push({type: terrain.type, pos: [x+i, y+i], range: range});}
-				if(player == 'findoff'){visibleObj.fisab.push({type: terrain.type, pos: [x+i, y+i], range: range})};
+			var x = Math.cos(angle + 0.5*Math.PI);
+			var y = Math.sin(angle + 0.5*Math.PI);
+			for(var terrain of terrains){
+			if(	x*i+pos[0] < terrain.pos[0]+terrain.size[0] &&
+				x*i+pos[0] > terrain.pos[0] &&
+				y*i+pos[1] > terrain.pos[1] &&
+				y*i+pos[1] < terrain.pos[1]+terrain.size[1]
+				){
+				var xD = x*i + pos[0];
+				var yD = y*i + pos[1];
+				var range = Math.sqrt(xD*xD+yD*yD); //ФИКСИТЬ
+				ctx.beginPath();
+				ctx.fillStyle = 'red';
+				ctx.arc(xD, yD, 10, 0, Math.PI*2, 0);
+				ctx.fill();
+				//if(player.nick == 'fisab'){visibleObj.fisab.push({type: terrain.type, pos: [x+i, y+i], range: range});}
+				//if(player.nick == 'findoff'){visibleObj.findoff.push({type: terrain.type, pos: [x+i, y+i], range: range})};
+				return;
 			}
 		}
 	}
@@ -122,43 +146,73 @@ function sendAllInfo(){
 		allInfo.findoff.mePos = players[1].findoff;
 		allInfo.findoff.visibleObj = visibleObj.findoff;
 		allInfo.findoff.tick = tick;
-		//findoff.postMessage(allInfo.findoff);
+		findoff.postMessage(allInfo.findoff);
 	}
 }
 
-var withoutCollisionTop = 0;
 function moveForward(player){
+	var dx = -Math.cos(player.angle)*velocity.drive;
+	var dy = -Math.sin(player.angle)*velocity.drive;
+	var a = 0;
 	for(var terrain of terrains){
-		if(	(player.pos[0]-25 > terrain.pos[0]+terrain.size[0]) ||
-			player.pos[0]+25 < terrain.pos[0] &&
-			player.pos[1]+25 > terrain.pos[1]+terrain.size[1] ||
-			player.pos[1]-25 < terrain.pos[1]
-			){
-			withoutCollisionTop++;
+		a++;
+		var x = player.pos[0];
+		var y = player.pos[1];
+		var tx = terrain.pos[0];
+		var ty = terrain.pos[1];
+		var r = 25;
+		var txr = terrain.size[0];
+		var tyr = terrain.size[1];
+
+		for(var terrain of terrains)
+		{
+			for (var i = 0; i < w; i++) {
+
+			}
+		}
+
+		if( x + r + dx > tx &&
+			x - r + dx < tx + txr &&
+			y + r + dy > ty &&
+			y - r + dy < ty + tyr){
+			return;
 		}
 	}
-	if(withoutCollisionTop == terrains.length){
-		withoutCollisionTop = 0;
-		player.pos[0] -= Math.cos(player.angle)*velocity.drive;
-		player.pos[1] -= Math.sin(player.angle)*velocity.drive;
-	}
+	player.pos[0] += dx;
+	player.pos[1] += dy;
 }
 
 var withoutCollisionBot = 0;
 function moveBack(player){
+	var dx = -Math.cos(player.angle)*velocity.drive;
+	var dy = -Math.sin(player.angle)*velocity.drive;
+	var a = 0;
 	for(var terrain of terrains){
-		if(	Math.sin(player.angle+Math.PI)*velocity.drive > terrain.pos[1] &&
-			Math.sin(player.angle+Math.PI)*velocity.drive < terrain.pos[1] &&
-			Math.cos(player.angle+Math.PI)*velocity.drive > terrain.pos[0] &&
-			Math.cos(player.angle+Math.PI)*velocity.drive < terrain.pos[0]){
-			withoutCollisionBot++;
+		a++;
+		var x = player.pos[0];
+		var y = player.pos[1];
+		var tx = terrain.pos[0];
+		var ty = terrain.pos[1];
+		var r = 25;
+		var txr = terrain.size[0];
+		var tyr = terrain.size[1];
+
+		for(var terrain of terrains)
+		{
+			for (var i = 0; i < w; i++) {
+
+			}
+		}
+
+		if( x + r - dx > tx &&
+			x - r - dx < tx + txr &&
+			y + r - dy > ty &&
+			y - r - dy < ty + tyr){
+			return;
 		}
 	}
-	//if(withoutCollisionBot == terrains.length){
-		withoutCollisionBot = 0;
-		player.pos[0] += Math.cos(player.angle)*velocity.drive;
-		player.pos[1] += Math.sin(player.angle)*velocity.drive;
-	//}
+	player.pos[0] -= dx;
+	player.pos[1] -= dy;
 }
 
 function rotateGun(direction, player){
@@ -263,7 +317,9 @@ function processAction(){//раскладывает по полкам все з�
 
 function process(){
 	for(var player of players){
-		findObjForRadar(player.pos, player.angle, player);
+		//var player = players[0];
+		findObjForRadar(player.pos, player.gunAngle, player);
+		//radar(player.pos, player.gunAngle, player);
 	}
 	processAction();
 	sendAllInfo();
@@ -272,16 +328,16 @@ function process(){
 function render(){
 	ctx.clearRect(0,0, w,h);
 	for(var terrain of terrains){
-		if(terrain.type == 'wall'){
+		//if(terrain.type == 'wall'){
 			ctx.beginPath();
 	        ctx.fillStyle = '#000';
-	        ctx.strokeRect(terrain.pos[0], terrain.pos[1], terrain.size[0], terrain.size[1]);
+			ctx.fillRect(terrain.pos[0], terrain.pos[1], terrain.size[0], terrain.size[1]);
 	        ctx.fill();
-		}
+		//}
 	}
 
-	//for(var player of players){
-		player = players[0];
+	for(var player of players){
+		//player = players[0];
 
 	    ctx.save();
 	    ctx.beginPath();
@@ -290,7 +346,7 @@ function render(){
 	    ctx.translate(player.pos[0], player.pos[1]);
 	    ctx.rotate(player.angle);
 	    ctx.moveTo(0,0);
-	    ctx.lineTo(-25, 0);
+	    ctx.lineTo(-24, 0);
 	    ctx.stroke();
 	    ctx.restore();
 
@@ -298,12 +354,9 @@ function render(){
 		ctx.beginPath();
 		ctx.lineWidth = 5;
 		ctx.strokeStyle = player.color;
-	    ctx.translate(player.pos[0], player.pos[1]);
-	    ctx.rotate(player.angle);
-	    ctx.strokeRect(-25,-25,50,50);
+		ctx.arc(player.pos[0], player.pos[1], 25, 0, 2*Math.PI, false);
 	    ctx.stroke();
 	    ctx.restore();
-
 
 	    ctx.save();
 	    ctx.beginPath();
@@ -318,18 +371,18 @@ function render(){
 	    ctx.translate(player.pos[0], player.pos[1]);
 	    ctx.rotate(player.gunAngle);
 		ctx.lineTo(-5, 0);
-		ctx.lineTo(-5, 35);
-		ctx.lineTo(5, 35);
+		ctx.lineTo(-5, 45);
+		ctx.lineTo(5, 45);
 		ctx.lineTo(5, 0);
 		ctx.lineTo(0, 0);
 	    ctx.fill();
 	    ctx.restore();
-	//}
+	}
 }
 
 function loop(){
-	process();
 	render();
+	process();
 }
 
 setInterval(function(){loop();}, 16);
